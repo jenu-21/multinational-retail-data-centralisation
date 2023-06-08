c3c4fbfc1e79226f6547ca9b51721e4e125ee7f9
Jenushan Jayarooban <jenu21@Jenushans-MacBook-Pro.local>
2023-06-08 14:18:34 +0100

    Updated code for data pipeline

--- data_pipeline.py
+++ data_pipeline.py
@@ -7,16 +7,29 @@ db_connector = DatabaseConnector()
 data_extractor = DataExtractor()
 data_cleaner = DataCleaning()
 
-# Extract data from RDS database
-tables = db_connector.list_db_tables('db_creds.yaml')
-user_table = 'user_data'  # Assuming the user data is in a table named 'user_data'
-if user_table in tables:
-    user_data = data_extractor.read_rds_table(db_connector, user_table)
+number_of_stores_endpoint = 'https://aqj7u5id95.execute-api.eu-west-1.amazonaws.com/prod/number_stores'
+headers = {'x-api-key': 'yFBQbwXe9J3sd6zWVAMrK6lcxxr0q1lr2PT6DDMX'}
+number_of_stores = data_extractor.list_number_of_stores(number_of_stores_endpoint, headers)
+print(f"Number of stores to extract: {number_of_stores}")
 
-    # Cleaning the user data
-    cleaned_user_data = data_cleaner.clean_user_data(user_data)
+store_endpoint = 'https://aqj7u5id95.execute-api.eu-west-1.amazonaws.com/prod/store_details/{store_number}'
+print(f"Number of stores: {number_of_stores}")
 
-    # Upload cleaned data to the database
-    db_connector.upload_to_db(cleaned_user_data, 'dim_users')
-else:
-    print(f"Table '{user_table}' not found in the database.")
+store_data = data_extractor.retrieve_stores_data(store_endpoint, number_of_stores)
+
+cleaned_store_data = data_cleaner.clean_store_data(store_data)
+
+
+# link = 'https://data-handling-public.s3.eu-west-1.amazonaws.com/card_details.pdf'
+# pdf_data = data_extractor.retrieve_pdf_data(link)
+
+# tables = db_connector.list_db_tables()
+
+# pdf_data = data_extractor.retrieve_pdf_data(link)
+
+# cleaned_data = data_cleaner.clean_card_data(pdf_data)
+
+table_name = 'dim_store_details'
+db_connector.upload_to_db(cleaned_store_data, table_name)
+
+
