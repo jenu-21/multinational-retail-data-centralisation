eb59d43695ffa8c64f5e375aebf40d9353d1918a
Jenushan Jayarooban <jenu21@Jenushans-MacBook-Pro.local>
2023-06-08 20:50:15 +0100

    Updated code for data pipeline

--- data_pipeline.py
+++ data_pipeline.py
@@ -8,16 +8,26 @@ data_extractor = DataExtractor()
 data_cleaner = DataCleaning()
 
 number_of_stores_endpoint = 'https://aqj7u5id95.execute-api.eu-west-1.amazonaws.com/prod/number_stores'
+store_endpoint = 'https://aqj7u5id95.execute-api.eu-west-1.amazonaws.com/prod/store_details/{}'
 headers = {'x-api-key': 'yFBQbwXe9J3sd6zWVAMrK6lcxxr0q1lr2PT6DDMX'}
+
 number_of_stores = data_extractor.list_number_of_stores(number_of_stores_endpoint, headers)
+if number_of_stores is None:    
+    print("Unable to retreive the number of stores. Exiting...")
+    exit()
+
 print(f"Number of stores to extract: {number_of_stores}")
 
-store_endpoint = 'https://aqj7u5id95.execute-api.eu-west-1.amazonaws.com/prod/store_details/{store_number}'
-print(f"Number of stores: {number_of_stores}")
+store_data = data_extractor.retrieve_stores_data(store_endpoint, number_of_stores, headers)
+if store_data is None:
+    print("Unable to retrieve store data. Exiting...")
+    exit()
 
-store_data = data_extractor.retrieve_stores_data(store_endpoint, number_of_stores)
+print(f"Number of stores extracted: {len(store_data)}")
 
 cleaned_store_data = data_cleaner.clean_store_data(store_data)
+print("Cleaned store data:")
+print(cleaned_store_data)
 
 
 # link = 'https://data-handling-public.s3.eu-west-1.amazonaws.com/card_details.pdf'
