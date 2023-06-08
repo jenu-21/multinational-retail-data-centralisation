7ee820fc2d3590fabad76c154bfc0910be7d499e
Jenushan Jayarooban <jenu21@Jenushans-MacBook-Pro.local>
2023-06-08 20:53:33 +0100

    Updated data extraction code

--- data_extraction.py
+++ data_extraction.py
@@ -17,21 +17,28 @@ class DataExtractor:
         extracted_data = pd.concat(pdf_wrapper)   
         return extracted_data
     
+    def __init__(self):
+        self.headers = {'x-api-key': 'yFBQbwXe9J3sd6zWVAMrK6lcxxr0q1lr2PT6DDMX'}
+    
     def list_number_of_stores(self, endpoint, headers):
         response = requests.get(endpoint, headers=headers)
         print(f"Response: {response.json()}")
         if response.status_code == 200:
-            number_of_stores = response.json().get('count')
+            number_of_stores = response.json().get('number_stores')
             return number_of_stores
         else:
             print(f"Failed to retrieve store data. Status code: {response.status_code}")
             return None
 
-    def retrieve_stores_data(self, store_endpoint, number_of_stores):
+    def retrieve_stores_data(self, store_endpoint, number_of_stores, headers):
+        if number_of_stores is None:
+            print("Number of stores is not available. Cannot retrieve store data")
+            return None
+        
         stores_data = []
         for store_number in range(1, number_of_stores + 1):
             url = store_endpoint.format(store_number)
-            response = requests.get(url)
+            response = requests.get(url, headers = headers)
             if response.status_code == 200:
                 store_data = response.json()
                 stores_data.append(store_data)
