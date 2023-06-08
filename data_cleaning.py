2d7f4829c50ccc4b1fe87e32e8ab09fe134d3cc9
Jenushan Jayarooban <jenu21@Jenushans-MacBook-Pro.local>
2023-06-08 14:18:56 +0100

    Updated data cleaning code

--- data_cleaning.py
+++ data_cleaning.py
@@ -6,16 +6,33 @@ class DataCleaning:
         
         # Drop rows with NULL values
         cleaned_data.dropna(inplace=True)
-        
-        # Clean date errors and convert to datetime
-        cleaned_data['date'] = pd.to_datetime(cleaned_data['date'], errors='coerce')
-        
-        # Clean incorrectly typed values
-        cleaned_data['age'] = cleaned_data['age'].astype(int)
-        
-        # Remove rows with wrong information
-        cleaned_data = cleaned_data[cleaned_data['age'] > 0]  # Example: Age should be positive
+         
+        return cleaned_data
+    
+    def clean_card_data(self, df):
+        cleaned_data = df.copy()
+
+        card_columns = ['card_number', 'expiry_date', 'card_provider', 'date_payment_confirmed']
+        cleaned_data.dropna(subset=card_columns, inplace=True)
+
+        cleaned_data['card_number'] = cleaned_data['card_number'].str.replace('\s+', '', regex=True)
+        cleaned_data['expiry_date'] = cleaned_data['expiry_date'].str.replace('\s+', '', regex=True)
+        cleaned_data['card_provider'] = cleaned_data['card_provider'].str.strip()
+        cleaned_data['date_payment_confirmed'] = pd.to_datetime(cleaned_data['date_payment_confirmed'], errors='coerce')
         
         return cleaned_data
+    
+    def clean_store_data(self, df):
+        cleaned_data = df.copy()
+
+        cleaned_data.drop_duplicates(inplace = True)
+
+        return cleaned_data
+    
+
+    
+
+
+
 
 
