bda42ebda7e8ee2bdd0ce9a89eb703359018bb50
Jenushan Jayarooban <jenu21@Jenushans-MacBook-Pro.local>
2023-06-09 21:53:15 +0100

    Updated data extraction code

--- data_extraction.py
+++ data_extraction.py
@@ -2,14 +2,16 @@ from sqlalchemy import create_engine, text
 import pandas as pd
 import tabula.io as tabula
 import requests
+import boto3
 
-class DataExtractor:            
+class DataExtractor:   
+
     def read_rds_table(self, db_connector, table_name):
         engine = db_connector.init_db_engine()
         query = f'SELECT * FROM {table_name}'
 
         df = pd.read_sql_query(sql=text(query), con=engine.connect())
-        # df = pd.read_sql_table(table_name, con = engine, index_col = 'index')
+        df = pd.read_sql_table(table_name, con = engine, index_col = 'index')
         return df
     
     def retrieve_pdf_data(self, link):
@@ -47,5 +49,55 @@ class DataExtractor:
 
         df = pd.DataFrame(stores_data)
         return df
+
+    
+    
+    def extract_from_s3(self, s3_address):
+        bucket_name, key = self.parse_s3_address(s3_address)
+
+        session = boto3.Session()
+        s3_client = session.client('s3')
+
+        local_file_path = 'products.csv'
+        print(f"Local file path: {local_file_path}")
+
+        s3_client.download_file(bucket_name, key, local_file_path)
+        print(f"File downloaded from S3")
+
+        df = pd.read_csv('products.csv')
         
+        return df
+    
+    def parse_s3_address(self, s3_address):
+        
+        s3_address = s3_address.replace('s3://', '')
+        parts = s3_address.split('/', 1)
+        bucket_name = parts[0]
+        key = parts[1] if len(parts) > 1 else ''
+
+        return bucket_name, key
+
+    def extract_from_s3(self, s3_address):
+        try:
+            response = requests.get(s3_address)
+            response.raise_for_status()
+            data = response.json()
+            df = pd.DataFrame(data)
+            return df
+        except Exception as e:
+            print(f"Failed to extract data from S3. Error: {e}")
+            return None  
+     
+    def parse_s3_address(self, s3_address):
+        s3_address = s3_address.replace('https://', '')
+        parts = s3_address.split('/', 1)
+        bucket_name = parts[0]
+        key = parts[1] if len(parts) > 1 else ''
+
+        return bucket_name, key
+            
+
+
+       
+
 
