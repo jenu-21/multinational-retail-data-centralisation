bf1b9f51de6f0bafbd2f734049da774298d50721
Jenushan Jayarooban <jenu21@Jenushans-MacBook-Pro.local>
2023-06-14 01:00:31 +0100

    Updated data pipeline code

--- data_pipeline.py
+++ data_pipeline.py
@@ -7,15 +7,11 @@ db_connector = DatabaseConnector()
 data_extractor = DataExtractor()
 data_cleaner = DataCleaning()
 
-
-
 s3_address = 'https://data-handling-public.s3.eu-west-1.amazonaws.com/date_details.json'
 date_data = data_extractor.extract_from_s3(s3_address)
 cleaned_date_data = data_cleaner.clean_date_data(date_data)
-
 table_name = 'dim_date_times'
 db_connector.upload_to_db(cleaned_date_data, table_name)
-
 
 s3_address = 's3://data-handling-public/products.csv'
 products_data = data_extractor.extract_from_s3(s3_address)
@@ -23,17 +19,15 @@ cleaned_products_data = data_cleaner.convert_product_weights(products_data)
 cleaned_products_data= data_cleaner.clean_products_data(cleaned_products_data)
 table_name = 'dim_products'
 db_connector.upload_to_db(cleaned_products_data, table_name)
-
 tables = db_connector.list_db_tables()
 print("Tables in the database:", tables)
 
 orders_table_name = 'orders_table'
 orders_data = data_extractor.read_rds_table(db_connector, orders_table_name)
-
 cleaned_orders_data = data_cleaner.clean_orders_data(orders_data)
-
 cleaned_table_name = 'orders_table'
 db_connector.upload_to_db(cleaned_orders_data, cleaned_table_name)
+
 
 number_of_stores_endpoint = 'https://aqj7u5id95.execute-api.eu-west-1.amazonaws.com/prod/number_stores'
 store_endpoint = 'https://aqj7u5id95.execute-api.eu-west-1.amazonaws.com/prod/store_details/{}'
@@ -64,6 +58,14 @@ pdf_data = data_extractor.retrieve_pdf_data(link)
 tables = db_connector.list_db_tables()
 pdf_data = data_extractor.retrieve_pdf_data(link)
 cleaned_data = data_cleaner.clean_card_data(pdf_data)
+table_name = 'dim_card_details'
+db_connector.upload_to_db(cleaned_data , table_name)
+
+
+users_table_name = 'legacy_users'
+users_data = data_extractor.read_rds_table(db_connector, users_table_name)
+cleaned_users_data = data_cleaner.clean_user_data(users_data)
+db_connector.upload_to_db(cleaned_users_data, users_table_name)
 
 
 
