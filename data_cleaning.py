8bb5fe2108cac454689cd24b86b862b767490b87
Jenushan Jayarooban <jenu21@Jenushans-MacBook-Pro.local>
2023-06-16 19:21:20 +0100

    Updated data cleaning code

--- data_cleaning.py
+++ data_cleaning.py
@@ -12,6 +12,9 @@ class DataCleaning:
     
     def isDigits(self,num):
         return str(num) if str(num).isdigit() else np.nan
+    
+    def remove_char_from_string(self,value):
+        return re.sub(r'\D', '',value)
 
 
     def clean_user_data(self, df):
@@ -37,15 +40,16 @@ class DataCleaning:
         cleaned_data = df.copy()
 
         cleaned_data['card_number'] = cleaned_data['card_number'].apply(str)
-        cleaned_data['card_number'] = cleaned_data['card_number'].str.replace('?','')
+        cleaned_data['card_number'] = cleaned_data['card_number'].str.replace('?', '', regex=False)
+
 
         # Remove rows with invalid card_number length
-        cleaned_data = cleaned_data[cleaned_data['card_number'].apply(lambda x: len(x) == 16)]
+        # cleaned_data = cleaned_data[cleaned_data['card_number'].apply(lambda x: len(x) == 16)]
 
         cleaned_data['date_payment_confirmed'] = pd.to_datetime(cleaned_data['date_payment_confirmed'], format='%Y-%m-%d', errors='coerce')
         cleaned_data['expiry_date'] = cleaned_data['expiry_date'].str.extract(r'(\d{2}/\d{2})')
-        cleaned_data.dropna(subset=['expiry_date'], inplace=True)
-        cleaned_data.dropna(how='all', inplace=True)
+        
+        cleaned_data.dropna(subset =['expiry_date'], how='any', inplace=True)
 
         # unique_data = cleaned_data['card_provider'].unique()
         # print(unique_data)
@@ -58,17 +62,17 @@ class DataCleaning:
 
         cleaned_data.drop(columns = 'lat', inplace=True)
         cleaned_data['opening_date'] = pd.to_datetime(cleaned_data['opening_date'], format='%Y-%m-%d', errors='coerce')
-        cleaned_data['staff_numbers'] = pd.to_numeric(cleaned_data['staff_numbers'], errors='coerce')
+        cleaned_data['staff_numbers'] = pd.to_numeric(cleaned_data['staff_numbers'].apply(self.remove_char_from_string) , errors='coerce')
 
-        unique_values_to_clean = ['NULL', '6FWDZHD7PW', '1ZVU03X2P6', '13KJZ890JH', '9IBH8Y4Z0S', 'NRQKZWJ9OZ', 'BIP8K8JJW2', 'ZCXWWKF45G', 'QP74AHEQT0', '1CJ5OAU4BR', 'YELVM536YT', 'QMAVR5H3LD', 'UBCIFQLSNY', 'Q1TJY8H1ZH', '2XE1OWOC23', '1T6B406CI8', 'QIUU9SVP51', 'SKBXAXF5G5', '7AHXLXIUEF', 'O0QJIRC943', '3ZR3F89D97', 'FP8DLXQVGH', 'LU3E036ZD9', 'RC99UKMZB2', '2YBZ1440V6', 'OXVE5QR07O', '6LVWPU1G64', 'Y8J0Z2W8O9', '2429OB3LMM', '0OLAK2I6NS', '50IB01SFAZ', 'L13EQEQODP', 'HMHIFNLOBN', '5586JCLARW', 'X349GIDWKU', 'O7NF1FZ74Y', 'VKA5I8H32X', 'RX9TCP2RGB', 'ISEE8A57FE',  '74BY7HSB6P',  'A3PMVM800J', '0RSNUU3DF5',  'J3BPB68Z1J', 'F3AO8V2LHU', 'GFJQ2AAEQ8', 'ZBGB54ID4H', 'SKO4NMRNNF',  'LACCWDI0SB', 'CQMHKI78BX', 'T0R2CQBDUS', 'GT1FO6YGD4', 'GMMB02LA9V', 'B4KVQB3P5Y', 'AJHOMDOHZ4', 'OH20I92LX3', 'SLQBD982C0', 'XTUAV57DP4', 'ID819KG3X5', 'A3O5CBWAMD', 'RY6K0AUE7F', 'TUOKF5HAAQ', 'FRTGHAA34B', '13PIY8GD1H',  'X0FE7E2EOG', 'AE7EEW4HSS', 'OYVW925ZL8', 'XQ953VS0FG', 'K0ODETRLS3', 'K8CXLZDP07', 'UXMWDMX1LC', '3VHFDNP8ET',	'9D4LK7X4LZ', 'D23PCWSM6S', '36IIMAQD58', 'NN04B3F6UQ',	'JZP8MIJTPZ', 'B3EH2ZGQAV', '1WZB1TE1HL',]
-        cleaned_data.replace(unique_values_to_clean, pd.NA, inplace=True)
+        unique_values_to_clean = ['NULL', '6FWDZHD7PW', '1ZVU03X2P6', '13KJZ890JH', '9IBH8Y4Z0S', 'NRQKZWJ9OZ', 'BIP8K8JJW2', 'ZCXWWKF45G', 'QP74AHEQT0', '1CJ5OAU4BR', 'YELVM536YT', 'QMAVR5H3LD', 'UBCIFQLSNY', 'Q1TJY8H1ZH', '2XE1OWOC23', '1T6B406CI8', 'QIUU9SVP51', 'SKBXAXF5G5', '7AHXLXIUEF', 'O0QJIRC943', '3ZR3F89D97', 'FP8DLXQVGH', 'LU3E036ZD9', 'RC99UKMZB2', '2YBZ1440V6', 'OXVE5QR07O', '6LVWPU1G64', 'Y8J0Z2W8O9', '2429OB3LMM', '0OLAK2I6NS', '50IB01SFAZ', 'L13EQEQODP', 'HMHIFNLOBN', '5586JCLARW', 'X349GIDWKU', 'O7NF1FZ74Y', 'VKA5I8H32X', 'RX9TCP2RGB', 'ISEE8A57FE',  '74BY7HSB6P',  'A3PMVM800J', '0RSNUU3DF5',  'J3BPB68Z1J', 'F3AO8V2LHU', 'GFJQ2AAEQ8', 'ZBGB54ID4H', 'SKO4NMRNNF',  'LACCWDI0SB', 'CQMHKI78BX', 'T0R2CQBDUS', 'GT1FO6YGD4', 'GMMB02LA9V', 'B4KVQB3P5Y', 'AJHOMDOHZ4', 'OH20I92LX3', 'SLQBD982C0', 'XTUAV57DP4', 'ID819KG3X5', 'A3O5CBWAMD', 'RY6K0AUE7F', 'TUOKF5HAAQ', 'FRTGHAA34B', '13PIY8GD1H',  'X0FE7E2EOG', 'AE7EEW4HSS', 'OYVW925ZL8', 'XQ953VS0FG', 'K0ODETRLS3', 'K8CXLZDP07', 'UXMWDMX1LC', '3VHFDNP8ET',	'9D4LK7X4LZ', 'D23PCWSM6S', '36IIMAQD58', 'NN04B3F6UQ',	'JZP8MIJTPZ', 'B3EH2ZGQAV', '1WZB1TE1HL', 'N/A']
+        cleaned_data.replace(unique_values_to_clean, np.nan, inplace=True)
 
         cleaned_data['continent'] = cleaned_data['continent'].str.strip()
         cleaned_data['continent'] = cleaned_data['continent'].replace({'eeEurope': 'Europe'})
         cleaned_data['continent'] = cleaned_data['continent'].replace({'eeAmerica': 'America'})
         
-        cleaned_data.dropna(how = 'any', inplace=True)
-        # cleaned_data.to_csv('dirty_store.csv')
+        cleaned_data.dropna(subset = ['staff_numbers'],how = 'any', inplace=True)
+        cleaned_data.to_csv('dirty_store.csv')
 
         return cleaned_data
     
@@ -103,7 +107,7 @@ class DataCleaning:
         cleaned_orders_data.drop(columns = 'level_0', inplace=True)
         cleaned_orders_data['card_number'] = cleaned_orders_data['card_number'].apply(self.isDigits)
 
-        cleaned_orders_data.dropna(how = 'all', inplace=True)
+        cleaned_orders_data.dropna(how = 'any', inplace=True)
         # columns_to_remove = ['first_name', 'last_name', '1', 'level_0']
         cleaned_orders_data.to_csv('clean_orders_data.csv')
 
